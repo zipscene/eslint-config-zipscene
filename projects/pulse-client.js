3cb244dc7a11d184e3faf7393193a0b2150f4a56
Rob Hilgefort <rjhilgefort@gmail.com>
2016-04-01 15:11:40 -0400

    turn off arrow-body-style

--- projects/pulse-client.js
+++ projects/pulse-client.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'zipscene/frameworks/ember',
   rules: {
+    "arrow-body-style": [0, "as-needed"],
     "ember/ember-extend-comments": [0, []],
     "ember/ember-reopenclass-comments": [0, []],
     "ember/ember-newline-extend": 0,
