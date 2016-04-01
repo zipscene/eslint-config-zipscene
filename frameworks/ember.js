16efccdcec9666caf92caf3c353e4b218cd9dc46
Rob Hilgefort <rjhilgefort@gmail.com>
2016-04-01 16:32:40 -0400

    Cleanup

--- frameworks/ember.js
+++ frameworks/ember.js
@@ -1,11 +1,9 @@
 module.exports = {
-  extends: 'zipscene/environments/frontend',
-  plugins: ["eslint-plugin-ember"],
+  extends: "zipscene/environments/frontend",
+  plugins: ["ember"/*, "ember-cleanup"*/],
   env: { "embertest": true },
   rules: {
-    "ember/ember-extend-comments": [2, [
-      "actions", "classNames"
-    ]],
+    "ember/ember-extend-comments": [2, ["actions", "classNames"]],
     "ember/ember-reopenclass-comments": [2, []],
     "ember/ember-newline-extend": 2,
     "ember/ember-newline-reopenclass": 2
