6b1c0eb215f23a809c1edfa90fa89cea31080f74
Rob Hilgefort <rjhilgefort@gmail.com>
2016-04-04 15:33:03 -0400

    break out core pulse rules

--- projects/pulse-client.js
+++ projects/pulse-client.js
@@ -1,7 +1,6 @@
 module.exports = {
-  extends: "zipscene/frameworks/ember",
+  extends: ["zipscene/frameworks/ember", "zipscene/projects/pulse"],
   rules: {
-    "arrow-body-style": [0, "as-needed"],
     "ember/ember-extend-comments": [0, []],
     "ember/ember-reopenclass-comments": [0, []],
     "ember/ember-newline-extend": 0,
