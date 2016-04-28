de190b0da0720c41783b6515b62aa33d58624bba
Rob Hilgefort <rjhilgefort@gmail.com>
2016-04-28 15:51:44 -0400

    allow for use of 'import' in browsers

--- environments/browser.js
+++ environments/browser.js
@@ -4,6 +4,9 @@ module.exports = {
 		'shared-node-browser': false,
 		'browser': true
 	},
+	parserOptions: {
+		"sourceType": "module"
+	},
 	rules: {
 		'indent': [ 2, 2, { 'SwitchCase': 1 } ],
 		'array-bracket-spacing': [ 2, 'never' ],
