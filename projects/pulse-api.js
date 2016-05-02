37f60fff945bf7db3b70b8dd60f60dd901228b6f
Rob Hilgefort <rjhilgefort@gmail.com>
2016-05-02 17:00:21 -0400

    updates after applying lodash linter to pulse

--- projects/pulse-api.js
+++ projects/pulse-api.js
@@ -1,7 +1,5 @@
 module.exports = {
 	extends: [ 'zipscene/environments/node', 'zipscene/projects/pulse' ],
 	rules: {
-		// TODO: test/lib/fake-data-utils.js#14:31 Must diable because it complains about filter
-		'lodash/prefer-lodash-method': [ 0, { 'ignoreObjects': [ 'pasync' ] } ]
 	}
 };
