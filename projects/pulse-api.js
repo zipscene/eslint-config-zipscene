f07417f2edfff69dea318fbbb19a9df4bfb2b0ff
Rob Hilgefort <rjhilgefort@gmail.com>
2016-05-02 15:25:29 -0400

    Have to ignore a lodash rule on api

--- projects/pulse-api.js
+++ projects/pulse-api.js
@@ -1,4 +1,7 @@
 module.exports = {
 	extends: [ 'zipscene/environments/node', 'zipscene/projects/pulse' ],
-	rules: {}
+	rules: {
+		// TODO: test/lib/fake-data-utils.js#14:31 Must diable because it complains about filter
+		'lodash/prefer-lodash-method': [ 0, { 'ignoreObjects': [ 'pasync' ] } ]
+	}
 };
