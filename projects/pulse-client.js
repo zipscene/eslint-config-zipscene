9df225bf419889d117491055b57934ca61d8e99e
Rob Hilgefort <rjhilgefort@gmail.com>
2016-06-08 11:45:01 -0400

    Fix up pulse lodash exceptions to apply to whole project

--- projects/pulse-client.js
+++ projects/pulse-client.js
@@ -4,7 +4,6 @@ module.exports = {
 		'ember/ember-extend-comments': [ 0, [] ],
 		'ember/ember-reopenclass-comments': [ 0, [] ],
 		'ember/ember-newline-extend': 0,
-		'ember/ember-newline-reopenclass': 0,
-		'lodash/no-extra-args': 0 // TODO: Write some rules for `lodash-extras` modded interface
+		'ember/ember-newline-reopenclass': 0
 	}
 };
--- projects/pulse.js
+++ projects/pulse.js
@@ -2,8 +2,9 @@ module.exports = {
 	extends: [ 'zipscene/modules/mocha', 'zipscene/modules/lodash', 'zipscene/modules/jsdoc' ],
 	rules: {
 		'arrow-body-style': [ 0, 'as-needed' ],
-		// TODO: test/lib/fake-data-utils.js#14:31 Must diable because it complains about filter
+		// TODO: test/lib/fake-data-utils.js#14:31 Must disable because it complains about filter.
 		//       Also an issue on client code with `$.find` when chained.
-		'lodash/prefer-lodash-method': [ 0, { 'ignoreObjects': [ 'pasync', 'Router', '$' ] } ]
+		'lodash/prefer-lodash-method': [ 0, { 'ignoreObjects': [ 'pasync', 'Router', '$' ] } ],
+		'lodash/no-extra-args': 0 // TODO: Write some rules for `lodash-extras` modded interface
 	}
 };
